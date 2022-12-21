09f9b2257c520dc5676cc2b333f081d0108c6860
SHAIK BAJI <92388095+shaikbaji003@users.noreply.github.com>
2022-12-21 21:42:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3038,3 +3038,146 @@ print("Priya", (value)=>{
 });
 console.log(2);
 -------------------------------------------------------------------------------------------
+
+  
+  
+  //21/12/2022
+  
+  ================================================================================================================================================================================
+Code 1: Remove Duplicate characters from String
+function removeDuplicateCharacters() {
+  var string='priya riya supriya'
+  let result= string.split('').filter((item, index, arr)=> {
+               return arr.indexOf(item) == index;
+               }).join('');
+  return result;
+}
+console.log(removeDuplicateCharacters());
+================================================================================================================================================================================
+Code 2: Remove Duplicate characters from array of element and find the count of an elements using set object
+var arr = [55, 44, 55,67,67,67,67,8,8,8,8,8,65,1,2,3,3,34,5];
+var unique = [...new Set(arr)]
+console.log(unique) //output: [55, 44, 67, 8, 65, 1, 2, 3, 34, 5]
+console.log(unique.length) //output: 10
+================================================================================================================================================================================
+Code 3: Remove Duplicate characters from array of element using filter
+var myArray = ['a', 1, 'a', 2, '1'];
+var unique = myArray.filter((value, index, arr) => arr.indexOf(value) === index);
+================================================================================================================================================================================
+Code 4:String reverse without reversing of individual words (Array of elements can be reverse with reverse() method but for string it is won't possible so required to split 
+and then join().
+function removeDuplicates(){
+   var string ="India is my country"
+   let result = string.split('').reverse().join('').split(' ').reverse().join(' ')
+   return result
+}
+console.log(removeDuplicates()) 
+output = "aidnI si ym yrtnuoc"
+------------------------------------------
+var reverseWords = function(s) {
+    let res = '';
+    let word = '';
+    for (let c of s) {
+        if (c === ' ') {
+            res += word + c;
+            word = '';
+        } else {
+            word = c + word;
+        }
+    }
+    return res + word;
+};
+console.log(reverseWords("priya bagde"))
+================================================================================================================================================================================
+Code 5:String reverse with reversing of individual words
+function withoutReverse(){
+   var string ="India is my country"
+   let result = string.split('').reverse().join('')
+   return result
+}
+console.log(withoutReverse())
+output = "yrtnuoc ym si aidnI"
+================================================================================================================================================================================
+Code 6:String reverse without using inbult function
+function Reverse(){
+   var string ="India is my country";
+   var result="";
+   for( var i=string.length-1; i>=0 ; i-- ) {
+      result=result+string[i] }
+   return result
+}
+console.log(Reverse())
+output = "yrtnuoc ym si aidnI"
+================================================================================================================================================================================
+Code 7: Find factorial of user input number
+const number = parseInt(prompt('Enter a positive integer: '));
+if (number < 0) { console.log('Error! Factorial for negative number does not exist.')}
+else if (number === 0) { console.log(`The factorial is 1.`)}
+else {
+    let fact = 1;
+    for (i = 1; i <= number; i++) {
+        fact *= i;
+    }
+    console.log(`The factorial is ${fact}.`);
+}
+================================================================================================================================================================================
+Code 8:Anagram
+function checkStringsAnagram() {
+var a="Army";
+var b="Mary"
+   let str1 =  a.toLowerCase().split('').sort().join('');
+   let str2 =  b.toLowerCase().split('').sort().join('');
+   if(str1 === str2){
+      console.log("True");
+   } 
+   else { 
+      console.log("False");
+   }
+}
+================================================================================================================================================================================
+Code 9: Swapping of 2 numbers with third variable
+let a=10;
+let b=20;
+let c;
+   c=a;
+   a=b;
+   b=c;
+console.log (a,b,c)
+================================================================================================================================================================================
+Code 10: Swapping of 2 numbers without third variable
+let a=10; 
+let b=20;
+   a=a+b //30
+   b=a-b //10
+   a=a-b //20
+console.log (a,b)
+================================================================================================================================================================================
+Code 11: To check the string or number is palindrome or not( ex: 121,madam,anna) using reverse method
+function checkPalindrome(){
+  const string = "anmna"
+  let arr= string.split('').reverse().join('')
+  //console.log(arr)
+  if (string==arr){
+      console.log("Palindrome")
+  }
+  else{
+      console.log("Not Palindrome")
+  }
+}
+checkPalindrome()
+================================================================================================================================================================================
+Code 12: To check the string or number is palindrome or not( ex: 121,madam,anna) using diving length by 2 and then comparing
+function checkPalindrome(){
+   const string = "12321"
+   let len = string.length;
+   for (i=0; i<len/2;i++){
+     if (string[i]!==string[len-1-i]){
+         console.log("Not Palindrome")
+     }
+     else{
+         console.log(" Palindrome")
+    }
+   }
+}
+checkPalindrome()
+================================================================================================================================================================================
